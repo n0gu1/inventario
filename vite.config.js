1d6bc58f117270742626827e84715eb6f72acee6
Cristian Gonzalez <82175293+n0gu1@users.noreply.github.com>
2025-10-28 03:18:45 -0600

    Fix Vite config for production

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/build/', // 👈 asegúrate de que Vite genere URLs relativas correctas
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // 👈 genera los assets y manifest aquí
+        manifest: true,         // 👈 Laravel necesita este manifest en producción
+        rollupOptions: {
+            input: 'resources/js/app.js', // 👈 punto de entrada
+        },
+    },
 });
